b265b8119ab973f8210abeb212a77cb86a764fca
KLT1234 <landt.kristoffer@gmail.com>
2022-08-15 13:43:24 +0200

    added to menu

--- files.py
+++ files.py
@@ -5,3 +5,20 @@ def menu():
     3. Remove
     4. Replace
     """
+
+    
+    return input("Enter choise: ")
+
+    if input == 1:
+        pass
+    elif input == 2:
+        pass
+    elif input == 3:
+        pass
+    elif input == 4:
+        pass
+    else:
+        exit()
+
+if __name__ == "__main__":
+    menu()
